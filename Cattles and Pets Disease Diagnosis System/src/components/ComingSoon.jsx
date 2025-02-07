8f1f486b3ead850811d8023e18d37d0d44e27fdf
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-02-07 23:47:00 +0530

    created the coming soon page

---
+++ Cattles and Pets Disease Diagnosis System/src/components/ComingSoon.jsx
@@ -0,0 +1,10 @@
+export default function ComingSoon() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-700 text-white fixed top-0  overflow-hidden">
+      <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
+      <p className="text-lg text-gray-400 mb-6">
+        We&apos;re working hard to bring something awesome!
+      </p>
+    </div>
+  );
+}
