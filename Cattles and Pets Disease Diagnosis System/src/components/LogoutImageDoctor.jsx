4a44f99ca272e43fd15b36c07a1f584f5bdbc7da
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-02-07 23:50:11 +0530

    created a doctor logout background image page

---
+++ Cattles and Pets Disease Diagnosis System/src/components/LogoutImageDoctor.jsx
@@ -0,0 +1,15 @@
+
+const LogoutImageDoctor = () => {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <img
+          src="https://i.pinimg.com/736x/00/39/7f/00397f95b8a51d225af097fadd878be8.jpg"
+          alt="Centered"
+          className="max-w-full max-h-full object-contain h-screen"
+        />
+      </div>
+    );
+  };
+  
+  export default LogoutImageDoctor;
+  
